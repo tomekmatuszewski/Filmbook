9279ff9124c30506c81093a242bcc42f9468a505
TomekM <tomasz_matuszewski@o2.pl>
2021-01-19 19:46:52 +0100

    basic schema finished

--- filmbook/urls.py
+++ filmbook/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from .views import HomeView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
     # path("", include("accounts.urls")),
     # path("", include("films.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ static/js/scroll_top_button.js
@@ -0,0 +1,14 @@
+var btn = $('#back_top_button');
+
+$(document).scroll(function() {
+  if ($(document).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '1000');
+});
